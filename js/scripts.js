ad99f1613b27247fcc07693984ffcc7fc2159236
Oleg Razin <razyax@gmail.com>
2020-07-23 16:59:35 +0300

    тестовое задание

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$( function() {
+    var dateFormat = "mm/dd/yy",
+        from = $( "#from" )
+            .datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                numberOfMonths: 1
+            })
+            .on( "change", function() {
+                to.datepicker( "option", "minDate", getDate( this ) );
+            }),
+        to = $( "#to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1
+        })
+            .on( "change", function() {
+                from.datepicker( "option", "maxDate", getDate( this ) );
+            });
+
+    function getDate( element ) {
+        var date;
+        try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+        } catch( error ) {
+            date = null;
+        }
+
+        return date;
+    }
+} );
+
+
+jQuery(document).ready(function() {
+    $(".input-reset-from").click(function() {
+
+        $("#from").val('');
+
+    });
+    $(".input-reset-to").click(function() {
+
+        $("#to").val('');
+
+    });
+
+});
+
+
